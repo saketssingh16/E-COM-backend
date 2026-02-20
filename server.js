b5a66357c8bf8040a010faf8525ee761bc3575fb
logSaketzero <saket.saurav@logzerotechnologies.com>
2026-02-20 09:50:44 +0530

    fixed db issue

--- server.js
+++ server.js
@@ -57,7 +57,10 @@ app.get("/", (req, res) => {
 // =========================
 // Start Server
 // =========================
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
